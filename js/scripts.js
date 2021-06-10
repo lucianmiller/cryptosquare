73bfabec3cbae19fa31963981be2f39a4379e560
Lucian <lucian.miller185@gmail.com>
2021-06-10 09:49:07 -0700

    It will take in users message and lowercase it

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
+    const userMessage = $("#message").val();
+    const lowerCaseMessage = userMessage.toLowerCase();
+    console.log(userMessage);
+    console.log(lowerCaseMessage);
   });
 });
