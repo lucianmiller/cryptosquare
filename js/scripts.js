cf56afbe439ee112a1aa620051a7b2b6acfd08c0
Lucian <lucian.miller185@gmail.com>
2021-06-11 11:54:24 -0700

    It will add fucntion to clean up input string

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
+//Business Logic----------
+function encryptMessage(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  const cleanedUpString = cleanUpInput(lowerCaseMessage);
+  console.log(cleanedUpString);
+  createSquare(cleanedUpString);
+};
+function cleanUpInput(inputString) {
+  let cleanString = "";
+  for (index = 0; inputString.length > index; index++) {
+    let currentCharCode = inputString.charCodeAt(index);
+    if (currentCharCode < 123 && currentCharCode > 96) {
+      cleanString += inputString.charAt(index);
+    };
+  };
+  return cleanString;
+};
+function createSquare(string) {
+  const inputLength = string.length;
+  //next step is to use Math.sqrt(inputLength) ask Dan later
+};
 
 //User Logic--------------
 $(document).ready(function() {
-  $("form#input").submit(function(event){
+  $("form#input").submit(function(event) {
     event.preventDefault();
     const userMessage = $("#message").val();
-    const lowerCaseMessage = userMessage.toLowerCase();
-    const encodedMessage = (encryptMessage(lowerCaseMessage));
-    console.log(userMessage);
-    console.log(lowerCaseMessage);
+    const encodedMessage = (encryptMessage(userMessage));
   });
 });
