29b6287403e1b85ce792a76f199469733401558c
Lucian <lucian.miller185@gmail.com>
2021-06-11 09:37:47 -0700

    It will call encryptMessage function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+
+//User Logic--------------
 $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
     const userMessage = $("#message").val();
     const lowerCaseMessage = userMessage.toLowerCase();
+    const encodedMessage = (encryptMessage(lowerCaseMessage));
     console.log(userMessage);
     console.log(lowerCaseMessage);
   });
