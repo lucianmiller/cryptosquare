dbc444539f37bf5c27c041ea68efd22cc8951acc
Lucian <lucian.miller185@gmail.com>
2021-06-17 10:59:37 -0700

    It will encrypt message and output result to the user

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function finalEncryptedMessage(message) {
   const cleanedUpString = cleanUpInput(lowerCaseMessage);
   const cryptoSquare = createSquare(cleanedUpString);
   const encryptedMessage = encryptMessage(cryptoSquare);
-  console.log(encryptedMessage);
+  return encryptedMessage;
 };
 
 function cleanUpInput(inputString) {
@@ -24,7 +24,7 @@ function createSquare(string) {
   const squareRoot = Math.sqrt(inputLength);
   const roundedSquareRoot = Math.floor(squareRoot);
   const squareArr = [];
-  for (index = 0; workingString.length != 0; index++){
+  for (index = 0; workingString.length != 0; index++) {
     const rowArr = workingString.split('', roundedSquareRoot);
     squareArr.push(rowArr);
     workingString = workingString.slice(roundedSquareRoot);
@@ -34,23 +34,33 @@ function createSquare(string) {
 
 function encryptMessage(inputArr) {
   let outputArr = [];
-  let fiveCharArr = [];
-  for (index = 0; index < inputArr.length; index++) {
-    inputArr.forEach(array => {
-      if (array[index] != undefined) {
-        fiveCharArr.push(array[index]);
+  let fiveCharString = '';
+  for (index = 0; index <= inputArr.length; index++) {
+    inputArr.forEach(rowArray => {
+      let character = rowArray[index];
+      if (fiveCharString.length == 5) {
+        outputArr.push(fiveCharString);
+        fiveCharString = '';
+      };
+      if (rowArray[index] != undefined) {
+        fiveCharString += rowArray[index];
+      } else if (rowArray[index] == undefined) {
+        outputArr.push(fiveCharString);
+        index = inputArr.length;
+        return;
       };
     });
   };
-  const finalMessage = outputArr.join();
+  const finalMessage = outputArr.join(' ');
   return finalMessage;
 };
 
 //User Logic--------------
-$(document).ready(function() {
-  $("form#input").submit(function(event) {
+$(document).ready(function () {
+  $("form#input").submit(function (event) {
     event.preventDefault();
     const userMessage = $("#message").val();
     const encodedMessage = (finalEncryptedMessage(userMessage));
+    $("#code").html('Here is your encrypted message: ' + encodedMessage);
   });
 });
